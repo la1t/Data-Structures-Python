43bc9329a02bd9ce3b21e25053695e9040dcb38b
Anatoly Gusev <gusev.tolia@yandex.ru>
2021-03-16 21:15:56 +0300

    change is_palindrom calculation way

--- deque/is_palindrome.py
+++ deque/is_palindrome.py
@@ -5,7 +5,12 @@ def is_palindrome(a_string):
     deque = Deque()
     for symb in a_string:
         deque.addTail(symb)
-    for symb in a_string:
-        if symb != deque.removeTail():
+    
+    first_symb = deque.removeFront()
+    last_symb = deque.removeTail()
+    while last_symb is not None:
+        if first_symb != last_symb:
             return False
+        first_symb = deque.removeFront()
+        last_symb = deque.removeTail()
     return True
