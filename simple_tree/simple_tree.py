a6ed69079c7ca300b1ed37a5dd6df6f61bdb2938
Anatoly Gusev <gusev.tolia@yandex.ru>
2021-03-26 01:28:01 +0300

    added tree

---
+++ simple_tree/simple_tree.py
@@ -0,0 +1,78 @@
+class SimpleTreeNode:
+	
+    def __init__(self, val, parent):
+        self.NodeValue = val # значение в узле
+        self.Parent = parent # родитель или None для корня
+        self.Children = [] # список дочерних узлов
+	
+class SimpleTree:
+
+    def __init__(self, root):
+        self.Root = root # корень, может быть None
+	
+    def AddChild(self, ParentNode, NewChild):
+        NewChild.Parent = ParentNode
+        ParentNode.Children.append(NewChild)
+  
+    def DeleteNode(self, NodeToDelete):
+        NodeToDelete.Parent.Children.remove(NodeToDelete)
+        NodeToDelete.Parent = None
+
+    def GetAllNodes(self):
+        if self.Root is None:
+            return []
+        result = self.get_all_nodes_for_subtree(self.Root)
+        return result
+
+    def get_all_nodes_for_subtree(self, node):
+        result = [node]
+        for child in node.Children:
+            result += self.get_all_nodes_for_subtree(child)
+        return result
+
+    def FindNodesByValue(self, val):
+        # ваш код поиска узлов по значению
+        if self.Root is None:
+            return []
+        result = self.find_nodes_by_value_for_subtree(self.Root, val)
+        return result
+    
+    def find_nodes_by_value_for_subtree(self, node, val):
+        result = []
+        if node.NodeValue == val:
+            result.append(node)
+        for child in node.Children:
+            result += self.find_nodes_by_value_for_subtree(child, val)
+        return result
+   
+    def MoveNode(self, OriginalNode, NewParent):
+        # ваш код перемещения узла вместе с его поддеревом -- 
+        # в качестве дочернего для узла NewParent
+        self.DeleteNode(OriginalNode)
+        self.AddChild(NewParent, OriginalNode)
+   
+    def Count(self):
+        # количество всех узлов в дереве
+        if self.Root is None:
+            return 0
+        return self.count_subtree(self.Root)
+
+    def count_subtree(self, node):
+        result = 1
+        for child in node.Children:
+            result += self.count_subtree(child)
+        return result
+
+    def LeafCount(self):
+        # количество листьев в дереве
+        if self.Root is None:
+            return 0
+        return self.leaf_count_subtree(self.Root)
+
+    def leaf_count_subtree(self, node):
+        if not node.Children:
+            return 1
+        result = 0
+        for child in node.Children:
+            result += self.leaf_count_subtree(child)
+        return result
---
+++ simple_tree/tests.py
@@ -0,0 +1,136 @@
+import pytest
+
+from .simple_tree import SimpleTree, SimpleTreeNode
+
+
+@pytest.fixture(scope="function")
+def root():
+    return SimpleTreeNode(1, None)
+
+
+@pytest.fixture(scope="function")
+def tree(root):
+    return SimpleTree(root)
+
+
+@pytest.fixture(scope='function')
+def nodes_lvl_1(tree, root):
+    nodes_lvl_1 = [
+        SimpleTreeNode(0, None),
+        SimpleTreeNode(1, None),
+    ]
+    tree.AddChild(root, nodes_lvl_1[0])
+    tree.AddChild(root, nodes_lvl_1[1])
+    return nodes_lvl_1
+
+
+@pytest.fixture(scope="function")
+def nodes_lvl_2_1(tree, nodes_lvl_1):
+    nodes_lvl_2_1 = [
+        SimpleTreeNode(0, None),
+        SimpleTreeNode(1, None)
+    ]
+    tree.AddChild(nodes_lvl_1[0], nodes_lvl_2_1[0])
+    tree.AddChild(nodes_lvl_1[0], nodes_lvl_2_1[1])
+    return nodes_lvl_2_1
+
+
+@pytest.fixture(scope="function")
+def nodes_lvl_2_2(tree, nodes_lvl_1):
+    nodes_lvl_2_2 = [
+        SimpleTreeNode(0, None),
+        SimpleTreeNode(1, None)
+    ]
+    tree.AddChild(nodes_lvl_1[1], nodes_lvl_2_2[0])
+    tree.AddChild(nodes_lvl_1[1], nodes_lvl_2_2[1])
+    return nodes_lvl_2_2
+
+
+def test_add_child():
+    root = SimpleTreeNode(0, None)
+    tree = SimpleTree(root)
+    new_node = SimpleTreeNode(1, None)
+
+    tree.AddChild(root, new_node)
+
+    assert root.Children == [new_node]
+    assert new_node.Parent == root
+
+
+def test_delete_node():
+    root = SimpleTreeNode(0, None)
+    tree = SimpleTree(root)
+    subroot_node = SimpleTreeNode(0, None)
+    tree.AddChild(root, subroot_node)
+    deletable_node = SimpleTreeNode(1, None)
+    tree.AddChild(root, deletable_node)
+    sub_nodes = [
+        SimpleTreeNode(2, None),
+        SimpleTreeNode(3, None),
+    ]
+    tree.AddChild(deletable_node, sub_nodes[0])
+    tree.AddChild(deletable_node, sub_nodes[1])
+
+    tree.DeleteNode(deletable_node)
+
+    assert root.Children == [subroot_node]
+    assert deletable_node.Parent is None
+
+
+def test_get_all_nodes_if_tree_is_empty():
+    tree = SimpleTree(None)
+    
+    assert tree.GetAllNodes() == []
+
+
+def test_get_all_nodes_if_there_are_some_nodes(tree, root, nodes_lvl_1, nodes_lvl_2_1):
+    expected_nodes = [root, *nodes_lvl_1, *nodes_lvl_2_1]
+
+    all_nodes = tree.GetAllNodes()
+
+    assert len(expected_nodes) == len(all_nodes)
+    for node in expected_nodes:
+        assert node in all_nodes
+
+
+def test_find_nodes_by_value(tree, root, nodes_lvl_1, nodes_lvl_2_1):
+    expected_nodes = [root, nodes_lvl_1[1], nodes_lvl_2_1[1]]
+    found_nodes = tree.FindNodesByValue(1)
+
+    assert len(expected_nodes) == len(found_nodes)
+    for node in expected_nodes:
+        assert node in found_nodes
+
+
+def test_find_nodes_by_value_if_tree_is_empty():
+    tree = SimpleTree(None)
+
+    assert tree.FindNodesByValue(1) == []
+
+
+def test_move_node(tree, root, nodes_lvl_1, nodes_lvl_2_1):
+    tree.MoveNode(nodes_lvl_1[1], nodes_lvl_2_1[1])
+
+    assert root.Children == [nodes_lvl_1[0]]
+    assert nodes_lvl_2_1[1].Children == [nodes_lvl_1[1]]
+    assert nodes_lvl_1[1].Parent == nodes_lvl_2_1[1]
+
+
+def test_count(tree, root, nodes_lvl_1, nodes_lvl_2_1, nodes_lvl_2_2):
+    assert tree.Count() == 7
+
+
+def test_count_if_tree_is_empty():
+    tree = SimpleTree(None)
+
+    return tree.Count() == 0
+
+
+def test_leaf_count(tree, root, nodes_lvl_1, nodes_lvl_2_1):
+    assert tree.LeafCount() == 3
+
+
+def test_leaf_count_if_tree_is_empty():
+    tree = SimpleTree(None)
+
+    assert tree.LeafCount() == 0
